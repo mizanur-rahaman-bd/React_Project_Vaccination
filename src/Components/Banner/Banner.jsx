e5521a4ac4e4a1ed3f08c1babbeb7923751401f0
mizanur-rahaman-bd <sumon1581@gmail.com>
2024-10-22 00:38:42 +0600

    Banner_Img_Flex

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
     <section className='banner'>
       <div className="container">
       <div className="banner_row">
-        <div className="text_img_row">
+        <div className="text_img_row lg:flex lg:justify-between">
           <div className="banner_text_col">
             <h2>Get Vaccinated. Boost your Immune System</h2>
             <h1>COVID-19 Vaccination
@@ -16,7 +16,7 @@ const Banner = () => {
                 <span className='text-Brand_color font-montstarate font-extrabold text-[24px] md:text-[36px] lg:text-[48px] mb-[20px] md:mb-[40px] lg:mb-[50px]'>Vaccination.ng</span>
                 <p>Vaccination.ng will help you find the nearest
                 centre for vaccination, in all 36 states in Nigeria.</p>
-                <div className='banner_button mb-[50px] flex gap-[20px]'>
+                <div className='banner_button mb-[50px] md:mb[55px] lg:mb-0 flex gap-[20px]'>
                   <CommonButton CommonButtonText={'Get Vaccine'} />
                   <CommonButton CommonButtonText={'Help Centre'} />
                 </div>
