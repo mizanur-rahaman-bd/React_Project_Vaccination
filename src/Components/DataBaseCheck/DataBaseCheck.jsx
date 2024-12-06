664e50dacfb6d6d63ea60f90bf3d0ead8bda21ba
mizanur-rahaman-bd <sumon1581@gmail.com>
2024-12-06 23:15:08 +0600

    database_check_running

--- src/Components/DataBaseCheck/DataBaseCheck.jsx
+++ src/Components/DataBaseCheck/DataBaseCheck.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './DataBaseCheck.css'
+import { Link } from 'react-router-dom'
 
 const DataBaseCheck = () => {
   return (
@@ -8,6 +9,14 @@ const DataBaseCheck = () => {
         <div className="data_base_row">
             <div className='dataBaseHeadingtext'>
                 <h3>Check your COVID-19 result on our Database</h3>
+            </div>
+            <div className="input_section">
+              <input placeholder='Okeowo' type="text" />
+              <input placeholder='NIK Number' type="text" />
+              <button>Check</button>
+            </div>
+            <div className='certificate_text'>
+              <h3>Need a certificate for your COVID-19 result? Please <Link to={''}>click <span className='underline'>here</span> </Link> </h3>
             </div>
 
         </div>
